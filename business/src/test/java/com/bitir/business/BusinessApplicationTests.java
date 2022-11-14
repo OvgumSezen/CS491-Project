57375cdce49aa90eb7db56e01bb4ad0f0311dcfc
OvgumSezen <ovgumcansezen@gmail.com>
2022-11-14 15:23:18 +0300

    Added the shell of the microservices.

---
+++ business/src/test/java/com/bitir/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitir.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
