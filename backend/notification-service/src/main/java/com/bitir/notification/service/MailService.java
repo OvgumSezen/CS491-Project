6d4500f57e710616066de10ad59f1cfd5e318dc3
OvgumSezen <ovgumcansezen@gmail.com>
2022-12-12 05:46:29 +0300

    Added the mailing functionality to the notification-service.

---
+++ backend/notification-service/src/main/java/com/bitir/notification/service/MailService.java
@@ -0,0 +1,25 @@
+package com.bitir.notification.service;
+
+import com.bitir.notification.dto.MailRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MailService {
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(MailRequest mailRequest) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(mailRequest.getReceiver());
+        simpleMailMessage.setSubject(mailRequest.getSubject());
+        simpleMailMessage.setText(mailRequest.getContent());
+
+        javaMailSender.send(simpleMailMessage);
+        log.info("Mail notification is sent.");
+    }
+}
